2f13b46b92160cb25d10bb6c9d72a9291be7d0ec
SharleyCode <13124680288@163.com>
2016-03-05 16:23:24 +0800

    Initial commit

---
+++ app/src/main/java/com/lx/jessica/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lx.jessica.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lx.jessica.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
