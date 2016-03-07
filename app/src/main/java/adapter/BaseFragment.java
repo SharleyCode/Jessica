fbf8406d64123df8d17dbf856fef67cb0ae0e290
lvhao <13652900200@163.com>
2016-03-07 10:04:06 +0800

    添加BaseActivity类，是所有Activity的基类，项目类所有Activity必须继承该类 添加BaseFragment类，是所有Fragment基类，项目类所有Fragment必须继承该类

---
+++ app/src/main/java/adapter/BaseFragment.java
@@ -0,0 +1,48 @@
+package adapter;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Fragment的基类，项目中所有的Fragment应该继承该类
+ * Created by lvhao on 2016/3/5 0005.
+ */
+public abstract class BaseFragment extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(contentResid(),container,false);
+    }
+
+    /**
+     * 该方法不属于Fragment的生命周期方法
+     * 该方法会紧跟着onCreateView方法被调用
+     * @param view
+     * @param savedInstanceState
+     */
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        init(view);
+        loadDatas();
+    }
+
+    /**
+     * 子类调用该方法，返回Fragment中需要显示的布局ID
+     * @return
+     */
+    protected abstract int contentResid();
+
+    /**
+     * 加载数据方法，由子类具体实现
+     */
+    private void loadDatas() {
+    }
+    /**
+     * 初始化Fragment,由子类具体实现
+     * @param view
+     */
+    private void init(View view) {
+    }
+}
