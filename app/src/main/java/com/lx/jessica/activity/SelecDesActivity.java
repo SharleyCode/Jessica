dbeacf2ec63885125f4c900d3fabe605730b1fe7
sunxny <Sunxnyvip@163.com>
2016-03-12 11:42:13 +0800

    目的地选择

---
+++ app/src/main/java/com/lx/jessica/activity/SelecDesActivity.java
@@ -0,0 +1,28 @@
+package com.lx.jessica.activity;
+
+import android.widget.ListView;
+
+import com.lx.jessica.R;
+import com.lx.jessica.base.BaseActivity;
+
+/***
+ *  目的地选择页面activity
+ **/
+public class SelecDesActivity extends BaseActivity{
+    private ListView lvDes;
+    @Override
+    protected int getViewResid() {
+        return R.layout.activity_selectdes;
+    }
+
+    @Override
+    protected void init() {
+        lvDes= (ListView) findViewById(R.id.lv_des);
+    }
+/**
+ * 下载数据
+  */
+    @Override
+    protected void loadDatas() {
+    }
+}
